c67d162a366077545aa763fdc8ea1223b19dc87c
noviandsh <noviandsh@gmail.com>
2022-04-12 13:09:58 +0700

    add social media icon

--- component/aside.js
+++ component/aside.js
@@ -1,12 +1,32 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faFacebookF, faTwitter, faInstagram } from "@fortawesome/free-brands-svg-icons"
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { faFacebookF, faTwitter, faInstagram, faGithub } from "@fortawesome/free-brands-svg-icons"
 
 export default function Aside() {
     return (
         <aside>
-            <FontAwesomeIcon icon={faFacebookF} size="10x" />
-            <FontAwesomeIcon icon={faTwitter} size="10x" />
-            <FontAwesomeIcon icon={faInstagram} size="10x" />
+            <ul>
+                <li>
+                    <a href="https://github.com/noviandsh">
+                        <FontAwesomeIcon icon={faGithub} size="xl" />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.facebook.com/noviandsh/">
+                        <FontAwesomeIcon icon={faFacebookF} size="xl" />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://twitter.com/noviandsh_">
+                        <FontAwesomeIcon icon={faTwitter} size="xl" />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/noviandsh/">
+                        <FontAwesomeIcon icon={faInstagram} size="xl" />
+                    </a>
+                </li>
+            </ul>
         </aside>
     )
 }
