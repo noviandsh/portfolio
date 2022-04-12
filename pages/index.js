13a21b154341297730a537b96abcd3352724788a
noviandsh <noviandsh@gmail.com>
2022-04-13 00:19:39 +0700

    add route animation with framer motion

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,26 @@
 import Layout from '../component/layout'
+import { motion } from 'framer-motion'
 
 export default function Home() {
-  return (
-    <Layout>
-      <h1>Novian Dwi Syahrizal Hilmi</h1>
-    </Layout>
-  )
+	return (
+		<Layout>
+			<motion.div id="greeting" initial="hidden" animate="visible" variants={{
+				hidden: {
+					scale: .8,
+					opacity: 0,
+				},
+				visible: {
+					scale: 1,
+					opacity: 1,
+					transition: {
+						delay: .2
+					}
+				}
+			}} >
+				<span>Hi, I'm</span>
+				<h1>Novian Dwi Syahrizal Hilmi.</h1>
+				<span>a Front-End Developer</span>
+			</motion.div>
+		</Layout>
+	)
 }
