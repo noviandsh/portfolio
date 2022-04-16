b33f3fa9c40abb508fd82d61991302c68df6976c
noviandsh <noviandsh@gmail.com>
2022-04-17 00:00:29 +0700

    add router progress

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,11 @@
+import Router from 'next/router'
 import '../styles/globals.css'
+import nprogress from 'nprogress'
+import "nprogress/nprogress.css"
+
+Router.events.on("routeChangeStart", nprogress.start)
+Router.events.on("routeChangeError", nprogress.done)
+Router.events.on("routeChangeComplete", nprogress.done)
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
