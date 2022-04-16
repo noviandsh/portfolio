9d5c0ffc964f7843efa012db1db3a316f4f4ec88
noviandsh <noviandsh@gmail.com>
2022-04-17 00:13:22 +0700

    add animation

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,61 @@
 import Layout from '../component/layout'
-import { motion } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
+import { useRouter } from 'next/router'
 
 export default function Home() {
+	const router = useRouter()
+	const variants = {
+		span1: {
+			hidden: {
+				y: -500,
+				opacity: 0
+			},
+			visible: {
+				y: 0,
+				opacity: 1,
+				transition: {
+					delay: .1,
+				}
+			}
+		},
+		h1: {
+			hidden: {
+				x: -500,
+				opacity: 0
+			},
+			visible: {
+				x: 0,
+				opacity: 1,
+				transition: {
+					delay: .3,
+				}
+			}
+		},
+		span2: {
+			hidden: {
+				x: 500,
+				opacity: 0
+			},
+			visible: {
+				x: 0,
+				opacity: 1,
+				transition: {
+					delay: .5,
+				}
+			}
+		},
+	}
 	return (
 		<Layout>
-			<motion.div id="greeting" initial="hidden" animate="visible" variants={{
-				hidden: {
-					scale: .8,
-					opacity: 0,
-				},
-				visible: {
-					scale: 1,
-					opacity: 1,
-					transition: {
-						delay: .2
-					}
-				}
-			}} >
-				<span>Hi, I'm</span>
-				<h1>Novian Dwi Syahrizal Hilmi.</h1>
-				<span>a Front-End Developer</span>
-			</motion.div>
+			<div id='greeting'>
+				<AnimatePresence>
+					<motion.span key="greet" initial="hidden" animate="visible" variants={variants.span1}>Hi, i'm</motion.span>
+					<motion.h1 key="name" initial="hidden" animate="visible" exit="hidden" variants={variants.h1}>
+						Novian Dwi Syahrizal Hilmi.
+					</motion.h1>
+					<motion.span key="works" initial="hidden" animate="visible" variants={variants.span2}>a Front-End Developer</motion.span>
+				</AnimatePresence>
+			</div>
 		</Layout>
 	)
 }
