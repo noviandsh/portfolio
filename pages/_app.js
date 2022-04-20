98370aa198cd8144e94a3ef799bb6f2429d99b5c
noviandsh <noviandsh@gmail.com>
2022-04-20 15:02:41 +0700

    fake fullscreen

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,14 @@ import Router from 'next/router'
 import '../styles/globals.css'
 import nprogress from 'nprogress'
 import "nprogress/nprogress.css"
+import { useEffect } from 'react'
 
 Router.events.on("routeChangeStart", nprogress.start)
 Router.events.on("routeChangeError", nprogress.done)
 Router.events.on("routeChangeComplete", nprogress.done)
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => window.scrollTo(0, 1))
   return <Component {...pageProps} />
 }
 
