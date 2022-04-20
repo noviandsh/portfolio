0ff5a7fca9a87febd0b46302d0039a065ab757d2
noviandsh <noviandsh@gmail.com>
2022-04-20 21:54:38 +0700

    fix hamburger menu when screen is larger

--- component/header.js
+++ component/header.js
@@ -5,15 +5,27 @@ import Nav from "./nav"
 
 export default function Header({ handleNavOpen, className }) {
     const [isNavOpen, setIsNavOpen] = useState(false)
+    const [windowWidth, setWindowWidth] = useState(0)
+
+    useEffect(() => {
+        const updateWidth = () => {
+            setWindowWidth(window.innerWidth)
+        }
+        window.addEventListener('resize', updateWidth)
+        updateWidth()
+        return () => window.removeEventListener('resize', updateWidth)
+    }, [])
 
     useEffect(() => {
         handleNavOpen(isNavOpen)
+        if (windowWidth > 768) {
+            setIsNavOpen(false)
+        }
     })
 
     const handleHamburgerClick = () => {
         setIsNavOpen(!isNavOpen)
     }
-
     return (
         <header className={className}>
             <div id="logo">&lt;NDSH/&gt;</div>
